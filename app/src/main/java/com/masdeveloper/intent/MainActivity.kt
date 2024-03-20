fc286005148a57dbd8dd3779d9356f19cd61b943
Miguel-Silva-Alves <miguelsilvalves.2015@gmail.com>
2024-03-20 11:35:53 -0300

    :sparkles: feat: add intent implicity2

--- app/src/main/java/com/masdeveloper/intent/MainActivity.kt
+++ app/src/main/java/com/masdeveloper/intent/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.masdeveloper.intent
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.masdeveloper.intent.Constantes.PARAMETRO_EXTRA
 import com.masdeveloper.intent.Constantes.PARAMETRO_REQUEST_CODE
 import com.masdeveloper.intent.databinding.ActivityMainBinding
@@ -13,9 +22,16 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private lateinit var parl: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        // Check to see if the BLE feature is available.
+        val bluetoothLEAvailable = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
+
+        Toast.makeText(this, bluetoothLEAvailable.toString(), Toast.LENGTH_SHORT).show()
 
         amb.mainTb.apply {
             title = getString(R.string.app_name)
@@ -27,25 +43,21 @@ class MainActivity : AppCompatActivity() {
 
             Intent("ABRA_PARAMETRO_ACTIVITY").also {
                 it.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString())
-                startActivityForResult(it, PARAMETRO_REQUEST_CODE)
+                parl.launch(it)
             }
-
-//            Intent(this, ParametroActivity::class.java).also {
-//                it.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString())
-//                startActivityForResult(it, PARAMETRO_REQUEST_CODE)
-//            }
         }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PARAMETRO_REQUEST_CODE){
-            if (resultCode == RESULT_OK){
-                if (data != null){
-                    val parametro = data.getStringExtra(PARAMETRO_EXTRA)
-                    amb.parametroTv.text = parametro
+        parl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                result.data?.getStringExtra(PARAMETRO_EXTRA)?.let {
+                    amb.parametroTv.text = it
                 }
             }
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
 }
