0cdc19c5c584a2488375e6617ba7aa1487e77be2
Miguel-Silva-Alves <miguelsilvalves.2015@gmail.com>
2024-03-20 10:34:29 -0300

    :sparkles: feat: add comunication between activits

--- app/src/main/java/com/masdeveloper/intent/MainActivity.kt
+++ app/src/main/java/com/masdeveloper/intent/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.masdeveloper.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.masdeveloper.intent.Constantes.PARAMETRO_EXTRA
+import com.masdeveloper.intent.Constantes.PARAMETRO_REQUEST_CODE
 import com.masdeveloper.intent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +21,25 @@ class MainActivity : AppCompatActivity() {
             title = getString(R.string.app_name)
             subtitle = this@MainActivity.javaClass.simpleName
             setSupportActionBar(this)
+        }
+
+        amb.entrarParametroBt.setOnClickListener{
+            Intent(this, ParametroActivity::class.java).also {
+                it.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString())
+                startActivityForResult(it, PARAMETRO_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PARAMETRO_REQUEST_CODE){
+            if (resultCode == RESULT_OK){
+                if (data != null){
+                    val parametro = data.getStringExtra(PARAMETRO_EXTRA)
+                    amb.parametroTv.text = parametro
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/masdeveloper/intent/ParametroActivity.kt
+++ app/src/main/java/com/masdeveloper/intent/ParametroActivity.kt
@@ -1,7 +1,10 @@
 package com.masdeveloper.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.masdeveloper.intent.Constantes.PARAMETRO_EXTRA
+import com.masdeveloper.intent.Constantes.PARAMETRO_REQUEST_CODE
 import com.masdeveloper.intent.databinding.ActivityParametroBinding
 
 class ParametroActivity : AppCompatActivity() {
@@ -12,12 +15,24 @@ class ParametroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_parametro)
+        setContentView(amb.root)
+
+        intent.getStringExtra(PARAMETRO_EXTRA)?.let {
+            amb.parametroEt.setText(it)
+        }
 
         amb.mainTb.apply {
             title = getString(R.string.app_name)
             subtitle = this@ParametroActivity.javaClass.simpleName
             setSupportActionBar(this)
+        }
+
+        amb.enviarParametroBt.setOnClickListener{
+            val parametro = amb.parametroEt.text.toString()
+            val resultIntent = Intent()
+            resultIntent.putExtra(PARAMETRO_EXTRA, parametro)
+            setResult(RESULT_OK, resultIntent)
+            finish()
         }
     }
 
